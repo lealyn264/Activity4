488c4db2f55b79e2d8070a0b6316a85b6037c303
lealyn264 <lealynmalco264@gmail.c om>
2023-11-10 13:12:11 +0800

    feat/insert-10-rows-owners

--- veterinary.sql
+++ veterinary.sql
@@ -48,7 +48,7 @@ CREATE TABLE treatments (
 CREATE TABLE invoices(
     invoiceid INT PRIMARY KEY,
     appointid INT FOREIGN KEY,
-    totalamount NUMERIC (10,2),
+    totalamount NUMERIC (10,2)
     paymentdate TIME
 )
 
@@ -59,5 +59,21 @@ CREATE TABLE mediacators(
     doctorid FOREIGN KEY,
     diagnosis TEXT,
     prescription TEXT,
-    notes TEXT
-)
+    notes TEXT,
+    FOREIGN KEY(animalid) REFERENCES animals(animalid),
+    FOREIGN KEY(doctorid) REFERENCES doctors(doctorid)
+);
+
+
+INSERT INTO owners(ownerid, ofirstname,olastname, address, phone, email)
+VALUES 
+(1, 'Juan', 'Dela Cruz', '123 Main St, Manila', '123-456-7890', 'juan@example.com'),
+(2, 'Maria', 'Santos', '456 Elm st, Quezon City', '987-654-3210', 'maria@example.com'),
+(3, 'Antonio', 'Gonzales', '789 Oak st, Cebu', '555-123-4567', 'antonio@example.com'),
+(4, 'Elena', 'Rodriguez', '345 Pine St, Davao', '777-888-9999', 'elena@examaple.com'),
+(5, 'Ricardo', 'Lim', '987 Cedar st, Makati', '222-333-4444', 'ricardo@examaple.com'),
+(6, 'isabel', 'Reyes', '111 Acacia st, Pasig', '999-000-1111', 'isabel@examaple.com'),
+(7, 'Luis', 'Torres', '555 Maple St, Mandaluyong', '123-555-7777', 'luis@examaple.com'),
+(8, 'Carmen', 'Fernandez', '222 Birch St, Taguig', '333-222-1111', 'carmen@examaple.com'),
+(9, 'Pedro', 'Santillan', '888 Spruce St, Bacolod', '888-777-6666', 'pedro@examaple.com'),
+(10, 'Sofia', 'Villanueva', '777 Walnut St, Iloilo', '111-999-3333', 'sofia@examaple.com');
