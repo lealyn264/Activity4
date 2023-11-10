4ca2cda0f55350dd9992e1c7faf5b508bedd6a82
JannieZurbano <janniebalbairazurbano@gmail.com>
2023-11-10 12:38:32 +0800

    feat/create-table-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -29,3 +29,17 @@ CREATE TABLE appointments (
     reason VARCHAR(255),
     FOREIGN KEY (animalid) REFERENCES animals(animalid)
 );
+
+CREATE TABLE doctors(
+    doctorid INT PRIMARY KEY,
+    dfirstname VARCHAR(50),
+    dlastname VARCHAR(50),
+    specialty VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
+
+CREATE TABLE treatments (
+    treatmentid INT PRIMARY KEY,
+    phonenumber VARCHAR(15),
+)
